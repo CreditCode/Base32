1e7aa76cab2d8a17317aafd84ea9cef242b3cf6c
faithful <31350738+CreditCode@users.noreply.github.com>
2019-02-17 17:21:20 +0100

    fixed a stupid name bug
    > print(py_url_gen(6))

--- urlgen.py
+++ urlgen.py
@@ -2,9 +2,10 @@
 import uuid
 
 def py_url_gen(string_length=10):
+    
     random = str(uuid.uuid4()) 
     random = random.replace("-","") #We dont want "-" in our code, so we replace it with null
     return random[0:string_length] 
 
 
-print(my_random_string(6)) 
+print(py_url_gen(6)) 
